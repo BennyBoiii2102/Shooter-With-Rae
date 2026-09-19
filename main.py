e41788799a9003829b2b8621ba30c03eb2d04ddc
BennyBoiii2102 <ben.e.elliott10@gmail.com>
2026-09-18 19:19:17 -0500

    I dunno what I did

--- main.py
+++ main.py
@@ -5,19 +5,31 @@ import main_functions as func
 def WindowInit():
     running = True
     func.init("display")
-    func.init("time")
+    func.init("font")
     screen = pygame.display.set_mode((1280, 720))
     pygame.display.set_caption('Shooter Game Made With Rae Heedick')
     clock = pygame.time.Clock()
+
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((10, 10, 10))
+
+    font = pygame.font.Font(None, 36)
+    text = font.render("Welcome to our game!", 1, (255, 255, 255))
+    textpos = text.get_rect()
+    textpos.centerx = background.get_rect().centerx
+    background.blit(text, textpos)
+    
+    return screen, clock, background
 
 
 def MainLoop():
     while True:
         quit = Loop.Game()
 
-        if quit == True:
+        if quit == False:
             break
 
 
-WindowInit()
+screen, clock, background = WindowInit()
 MainLoop()
